d01f090e9739828e651430ceeb4a8b15a16ee386
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2021-01-07 14:22:43 -0500

    Added script to js file

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
+
+document.getElementById("button1").addEventListener("click", function()){alert(`I'm Right`);}
+document.getElementById("button2").addEventListener("click", function()){alert(`NO, I'm Right`);}
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
 
