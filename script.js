beaecc449a008d668f857025f4d0129764efd2fc
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2021-01-07 14:33:28 -0500

    Fixed button method

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-document.getElementById("button1").addEventListener("click", function()){alert(`I'm Right`);}
-document.getElementById("button2").addEventListener("click", function()){alert(`NO, I'm Right`);}
+document.getElementById("button1").addEventListener("click", function(){alert(`I'm Right`);})
+document.getElementById("button2").addEventListener("click", function(){alert(`NO, I'm right!!`);})
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
 
