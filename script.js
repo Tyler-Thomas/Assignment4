3a1c9a70ca9e3a50cf97dccd328ecf3ec6b75bc6
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2021-01-07 14:50:52 -0500

    fixed first 2 functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById("button1").addEventListener("click",function(){alert(`I'm right!!`)});
+document.getElementById("button2").addEventListener("click",function(){alert(`NO, I'm right!!`)});
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
 let check= function(str){
 if(str==="12345678")
