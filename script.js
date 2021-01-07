98ff51ecb489f928748402fc1674470b1749c925
Tyler Thomas <tylerthomas327@gmail.com>
2021-01-07 14:11:20 -0500

    More script

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
+let check= function(str){
+if(str==="12345678")
+document.getElementsByTagName('h1')[0].innerText="Nice";
+else
+alert('Wrong Password');
+}
+document.getElementById("pwd").addEventListener("submit",check(document.getElementById("pwd").value));
 
