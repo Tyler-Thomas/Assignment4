027df71d7d273e601d8b1ff728400af078269d8d
Tyler Thomas <tylerthomas327@gmail.com>
2021-01-08 10:02:08 -0500

    I tried ok

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
 let check= function(str){
+console.log(str);
 if(str==="12345678")
 document.getElementsByTagName('h1')[0].innerText="Nice";
 else
