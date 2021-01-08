e71d0ce40abc7d8c0957d3f1e05187dcefe5b74e
Yuying Su <49736753+yuyingsu@users.noreply.github.com>
2021-01-08 10:32:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById("Hover").addEventListener("mouseover",function(){alert('I told you not to hover over me')});
-let check= function(str){
-console.log(str);
-if(str==="12345678")
-document.getElementsByTagName('h1')[0].innerText="Nice";
-else
-alert('Wrong Password');
-}
-document.getElementById("pwd").addEventListener("submit",check(document.getElementById("pwd").value));
+document.getElementById("form").addEventListener("submit", function(e) {
+  pass = document.getElementById("pwd").value;
+  if(pass=="12345678"){
+    e.preventDefault();
+    document.getElementsByTagName('h1')[0].innerHTML="Login Successful!"
+  }else{
+    alert("Wrong password");
+  }
+});
 
