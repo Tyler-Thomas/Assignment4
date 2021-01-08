646f701bb2d1eccb9ab1f032dd4dc33d993b5535
Yuying Su <49736753+yuyingsu@users.noreply.github.com>
2021-01-08 10:33:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("form").addEventListener("submit", function(e) {
   pass = document.getElementById("pwd").value;
   if(pass=="12345678"){
     e.preventDefault();
-    document.getElementsByTagName('h1')[0].innerHTML="Login Successful!"
+    document.getElementsByTagName('h1')[1].innerHTML="Login Successful!"
   }else{
     alert("Wrong password");
   }
